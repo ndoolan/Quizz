53482126add685a2591e2e25e9b6fd964a8e9ab7
Dwijesek <dwijesek@gmail.com>
2024-03-02 10:49:35 -0800

    frontend

--- dist/src/app/index.js
+++ dist/src/app/index.js
@@ -3,8 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("Hello World!");
+exports.App = void 0;
 const react_1 = __importDefault(require("react"));
-const react_dom_1 = __importDefault(require("react-dom"));
-const app_1 = require("./app");
-react_dom_1.default.render(react_1.default.createElement(app_1.App, { name: "Gian" }), document.getElementById("root"));
+const App = ({ name }) => {
+    return (react_1.default.createElement("div", null,
+        react_1.default.createElement("h1", null,
+            "Great job ",
+            name,
+            "!")));
+};
+exports.App = App;
