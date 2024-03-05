84ec2d1dc6785d72b3f71f979dfe0474e3837fa8
Nick Doolan <nickdoolan31@gmail.com>
2024-03-04 22:40:10 -0500

    AuthFinished/ND

--- server/auth/authn.routes.ts
+++ server/auth/authn.routes.ts
@@ -1,13 +1,19 @@
 import express from 'express';
 import { Request, Response } from 'express';
 // Import Service Funcs Here :)
+import { createUser, validateUser } from './authn.service';
 const authRouter = express.Router();
 
 // Sign Up
 authRouter.post('/signup', async (req: Request, res: Response) => {
   try {
-    // Insert User Creation Function Here
-    const user = await res.status(201).json('User was successfully created');
+    // Create User Function Params should be updated with valid fields
+    // Currently hardcoding others in service
+    const username = 'Eddy';
+    const password = '123';
+    const email = 'EddypleaseWork@gmail.com';
+    const user = await createUser(username, password, email);
+    res.status(201).json('User was successfully created');
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -16,8 +22,16 @@ authRouter.post('/signup', async (req: Request, res: Response) => {
 // Log In
 authRouter.post('/login', async (req: Request, res: Response) => {
   try {
+    const username = 'Eddy';
+    const password = '123';
     // Insert User Log In Function Here
-    const user = await res.status(200).json('Successfully logged in');
+    const validUser = await validateUser(username, password);
+    if (validUser) {
+      res.status(200).cookie('quizz', validUser);
+      res.status(200).json('Successfully logged In');
+    } else {
+      res.status(200).json('Invalid User Inputs');
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -26,19 +40,11 @@ authRouter.post('/login', async (req: Request, res: Response) => {
 // Log Out
 authRouter.post('/logout', async (req: Request, res: Response) => {
   try {
-    // Insert Clearing Cookies Func here
-    // Should Logging Out Redirect somewhere or just clear cookies
+    res.clearCookie('Quizz');
     res.status(200).json('Successfully logged out');
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
-
-// import {
-//   createRecording,
-//   getRecordingById,
-//   updateRecordingById,
-//   deleteRecordingById,
-// } from './recording.service';
 
 export default authRouter;
--- server/auth/authn.service.ts
+++ server/auth/authn.service.ts
@@ -1,21 +1,58 @@
 import { PrismaClient } from '@prisma/client';
+import { encryptPass, validatePass } from './bcrypt';
 const prisma = new PrismaClient();
 
-async function createUser(username, email, password) {
+// Create User
+async function createUser(username: string, password: string, email: string) {
   try {
-    // Create a new user with the provided information
+    // Create a new user with the provided information (HARDCODED OTHERS)
+    // Currently testing for bcrypt
     const newUser = await prisma.user.create({
       data: {
-        username: 'new_user',
-        email: 'new_user@example.com',
-        password: 'hashed_password', // Replace with hashed password
+        username: username,
+        email: email,
+        password: await encryptPass(password), // Replace with hashed password
         // Add any other desired values for optional fields, e.g.,
-        firstName: 'John',
-        lastName: 'Doe',
+        firstName: 'FurtherTests',
+        lastName: 'One',
         lastAnswered: new Date(),
       },
     });
+    console.log(newUser);
   } catch (error) {
-    next({error});
+    throw new Error(`Failed to create new user: ${error}`);
   }
 }
+
+// Validate User Creds
+async function validateUser(username: string, password: string) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: username, // bccrypt password to compare
+      },
+    });
+
+    if (!user) {
+      return false;
+    } else {
+      const validPassword = await validatePass(password, user.password);
+      if (validPassword) {
+        const { username, firstName, lastName, lastAnswered, email } = user;
+        const userInformation = {
+          username,
+          firstName,
+          lastName,
+          lastAnswered,
+          email,
+        };
+        return userInformation;
+      }
+    }
+    // TODO assign cookies to user browser
+  } catch (error) {
+    throw new Error(`Failed to valid credentials: ${error}`);
+  }
+}
+
+export { createUser, validateUser };
