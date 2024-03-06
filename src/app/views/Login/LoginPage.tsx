56cd1390c455ae344ad75239b31fc4470286ceec
Dwijesek <dwijesek@gmail.com>
2024-03-05 18:36:26 -0800

    css file

--- src/app/views/Login/LoginPage.tsx
+++ src/app/views/Login/LoginPage.tsx
@@ -1,17 +1,18 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useAppDispatch } from "../../../redux/hooks/hook";
-import { login } from "../../../redux/slices/authSlice";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch } from '../../../redux/hooks/hook';
+import { login } from '../../../redux/slices/authSlice';
+import './login.css';
 
 const Login = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const onSubmit = (e) => {
     dispatch(login({ email, password })).then((action) => {
-      localStorage.setItem("accessToken", action.payload.token);
-      navigate("/");
+      localStorage.setItem('accessToken', action.payload.token);
+      navigate('/');
     });
   };
 
@@ -20,27 +21,27 @@ const Login = () => {
       <h1>Login</h1>
       <form onSubmit={onSubmit}>
         <div>
-          <label htmlFor="email">Email</label>
+          <label htmlFor='email'>Email</label>
           <input
-            type="text"
-            id="email"
-            placeholder="Email"
-            name="email"
+            type='text'
+            id='email'
+            placeholder='Email'
+            name='email'
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
         <div>
-          <label htmlFor="password">Password</label>
+          <label htmlFor='password'>Password</label>
           <input
-            type="password"
-            id="password"
-            placeholder="Password"
-            name="password"
+            type='password'
+            id='password'
+            placeholder='Password'
+            name='password'
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div>
-          <button type="submit">Sign In</button>
+          <button type='submit'>Sign In</button>
         </div>
       </form>
     </div>
